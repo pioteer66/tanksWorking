c9932ac83ce84a833651ae7fba250b7c24232b5c
KB1101 <kornelgcc@gmail.com>
2016-04-13 14:11:43 +0200

    robie strzelanie

--- core/src/model/Pocisk.java
+++ core/src/model/Pocisk.java
@@ -4,7 +4,7 @@ package model;
  * Created by Dawid on 2016-04-12.
  */
 public class Pocisk {
-    private int startowyX, startowyY, koncowyX,koncowyY;
+    private int startowyX, startowyY, koncowyX,koncowyY,polX, polY;
     private Czolg wystrzelil;
     private int idPocisku;
 
@@ -13,7 +13,22 @@ public class Pocisk {
         this.startowyY = sY;
         this.koncowyX = kX;
         this.koncowyY = kY;
-        
+        this.polX  = sX;
+        this.polY = sY;
         // tds
     }
+
+    public boolean Aktualizuj(){
+        if (this.startowyX == this.koncowyX && this.startowyY == this.koncowyY ) return false;
+
+        if(this.startowyX == this.koncowyX){
+            if(this.startowyY > this.koncowyY) this.startowyY--;
+            else if(this.startowyY < this.koncowyY) this.startowyY++;
+        } else if (this.startowyY == this.koncowyY){
+            if(this.startowyX > this.koncowyX) this.startowyX--;
+            else if(this.startowyX < this.koncowyY) this.startowyX++;
+        }
+        return true;
+    }
+
 }
