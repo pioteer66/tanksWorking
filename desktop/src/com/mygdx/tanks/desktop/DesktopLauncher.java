cf94cbd357a5fcb950e4a8377becda5699853a70
Piotr <pioteer.66@gmail.com>
2016-04-13 18:00:08 +0200

    zmieniono tekstury

--- desktop/src/com/mygdx/tanks/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/tanks/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.tanks.TanksGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void	main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height= 800;
