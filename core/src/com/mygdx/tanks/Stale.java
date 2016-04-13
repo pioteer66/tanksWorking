832a0c3a98cc14c0f1740e32e2d5241797f828e4
Piotr <pioteer.66@gmail.com>
2016-04-13 19:06:37 +0200

    Wstępna wersja kolizji

--- core/src/com/mygdx/tanks/Stale.java
+++ core/src/com/mygdx/tanks/Stale.java
@@ -7,4 +7,5 @@ public class Stale {
     public static final int CZOLG_START_Y = 10/2;
     public static final int ROZMIAR_CZOLGU = 50;
     public static final int ROZMIAR_BLOKU = 25;
+    public static final int PREDKOSC_CZOLGU = 2;
 }
